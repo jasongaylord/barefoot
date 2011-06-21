060791afba0e393bb8a37672a6f823bc2eefe04f
Jason N. Gaylord <jasongaylord@users.noreply.github.com>
2011-06-20 23:07:39 -0400

    Added the capability of creating a strongly typed GPX document. It currently saves as temp.xml to the local store, but doesn't do anything else.

--- Barefoot/Core/Distance.cs
+++ Barefoot/Core/Distance.cs
@@ -14,23 +14,28 @@ namespace Barefoot.Core
             ////var coord1 = new GeoCoordinate(sLatitude, sLongitude);
             ////var coord2 = new GeoCoordinate(eLatitude, eLongitude);
             ////var meters = coord1.GetDistanceTo(coord2);
-            /// 
-            /// 1 meter = 0.00062137119 miles
-            /// http://www.dreamincode.net/code/snippet2414.htm
+            //// 1 meter = 0.00062137119 miles
+            //// http://www.dreamincode.net/code/snippet2414.htm
 
-            var sLatitudeRadians = sLatitude * (Math.PI / 180.0);
-            var sLongitudeRadians = sLongitude * (Math.PI / 180.0);
-            var eLatitudeRadians = eLatitude * (Math.PI / 180.0);
-            var eLongitudeRadians = eLongitude * (Math.PI / 180.0);
+            //var sLatitudeRadians = sLatitude * (Math.PI / 180.0);
+            //var sLongitudeRadians = sLongitude * (Math.PI / 180.0);
+            //var eLatitudeRadians = eLatitude * (Math.PI / 180.0);
+            //var eLongitudeRadians = eLongitude * (Math.PI / 180.0);
 
-            var dLongitude = eLongitudeRadians - sLongitudeRadians;
-            var dLatitude = eLatitudeRadians - sLatitudeRadians;
+            //var dLongitude = eLongitudeRadians - sLongitudeRadians;
+            //var dLatitude = eLatitudeRadians - sLatitudeRadians;
 
-            var result1 = Math.Pow(Math.Sin(dLatitude / 2.0), 2.0) + Math.Cos(sLatitudeRadians) * Math.Cos(eLatitudeRadians) * Math.Pow(Math.Sin(dLongitude / 2.0), 2.0);
+            //var result1 = Math.Pow(Math.Sin(dLatitude / 2.0), 2.0) + Math.Cos(sLatitudeRadians) * Math.Cos(eLatitudeRadians) * Math.Pow(Math.Sin(dLongitude / 2.0), 2.0);
 
-            var result2 = 2.0 * Math.Atan2(Math.Sqrt(result1), Math.Sqrt(1.0 - result1));
+            //var result2 = 2.0 * Math.Atan2(Math.Sqrt(result1), Math.Sqrt(1.0 - result1));
 
-            return (EarthRadiusMiles * result2);
+            //return (EarthRadiusMiles * result2);
+
+            var coord1 = new GeoCoordinate(sLatitude, sLongitude);
+            var coord2 = new GeoCoordinate(eLatitude, eLongitude);
+            var meters = coord1.GetDistanceTo(coord2);
+
+            return meters*0.00062137119;
         }
 
         ////// Javascript function to calculate
