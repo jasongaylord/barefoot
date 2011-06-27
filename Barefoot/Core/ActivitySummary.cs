ae596051fd55307843960d129d2fba5f938bb83c
Jason Gaylord <jgaylord@unitedoneresources.com>
2011-06-26 21:10:08 -0400

    Added support folder for tracking items used in building the app. Also, the ActivitySummary has been added to track the user history. Still need to implement adding a new item (and removing an item) though.

--- Barefoot/Core/ActivitySummary.cs
+++ Barefoot/Core/ActivitySummary.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace Barefoot.Core
 {
@@ -23,6 +27,68 @@ namespace Barefoot.Core
 
     public class ActivitySummaryCollection
     {
-        public ActivitySummary[] Collection { get; set; }
+        public Dictionary<DateTime, ActivitySummary> Collection { get; set; }
+        //public ActivitySummary[] Collection { get; set; }
+    }
+
+    public class ActivitySummaryFile
+    {
+        private const string Filename = "UserHistory.xml";
+
+        public ActivitySummaryCollection ReadFile()
+        {
+            var userStore = IsolatedStorageFile.GetUserStoreForApplication();
+
+            if (userStore.FileExists(Filename))
+            {
+                var file = userStore.OpenFile(Filename, FileMode.Open);
+
+                ActivitySummaryCollection activitySummaryCollection;
+
+                var xmlSerializer = new XmlSerializer(typeof (ActivitySummaryCollection));
+                activitySummaryCollection = (ActivitySummaryCollection)xmlSerializer.Deserialize(file);
+
+                return activitySummaryCollection;
+            }
+
+            return new ActivitySummaryCollection();
+        }
+
+        public bool WriteFile(ActivitySummaryCollection activitySummaryCollection)
+        {
+            var rtn = true;
+
+            try
+            {
+                var userStore = IsolatedStorageFile.GetUserStoreForApplication();
+                var writer = (!userStore.FileExists(Filename))
+                                 ? userStore.CreateFile(Filename)
+                                 : userStore.OpenFile(Filename, FileMode.Truncate);
+            
+                var xmlSerializer = new XmlSerializer(typeof(ActivitySummaryCollection));
+                xmlSerializer.Serialize(writer, activitySummaryCollection);
+
+                writer.Close();
+            }
+            catch (Exception)
+            {
+                rtn = false;
+                MessageBox.Show("Unfortunately, we're having trouble saving your history to memory. Try again later.",
+                                "Error", MessageBoxButton.OK);
+            }
+
+            return rtn;
+        }
+
+        public bool AddRecentActivity(ActivitySummary activitySummary)
+        {
+            var userHistory = ReadFile();
+
+            if (userHistory.Collection.Count > 49)
+            {
+                // TODO: Remove the last item and then insert this item
+                //userHistory.Collection.
+            }
+        }
     }
 }
